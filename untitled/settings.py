347f91145d066a62b8c0c179087e405c1f126b6a
Suraj Pai <surajballambat@gmail.com>
2017-05-25 13:37:29 +0530

    staticfix2

--- untitled/settings.py
+++ untitled/settings.py
@@ -127,3 +127,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
