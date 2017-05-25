6c74668fb6c3484869fd36b921b5e555ba8cf779
Suraj Pai <surajballambat@gmail.com>
2017-05-26 03:44:39 +0530

    added fn

--- untitled/static/script.js
+++ untitled/static/script.js
@@ -34,12 +34,12 @@
                             else if ( val == "p"){
 
                                 var input = $('#inputfield'+counter.toString());
-                                input.focus().val("").blur().focus().val('cat projects.txt').focus();
+                                input.focus().val("").blur().focus().val('cat projects.txt');
 
                             }
                             else if ( val == "e"){
 
-                                $('#inputfield'+counter.toString()).val('cat experience.txt');
+                                $('#inputfield'+counter.toString()).val('cat experience.txt').putCursorAtEnd();
                             }
                             else {
 
@@ -166,22 +166,34 @@
     }
 
 
-    jQuery.fn.putCursorAtEnd = function() {
-  return this.each(function() {
-    $(this).focus()
-    // If this function exists...
-    if (this.setSelectionRange) {
-      // ... then use it (Doesn't work in IE)
-      // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
-      var len = $(this).val().length * 2;
-      this.setSelectionRange(len, len);
-    } else {
-      // ... otherwise replace the contents with itself
-      // (Doesn't work in Google Chrome)
-      $(this).val($(this).val());
-    }
-    // Scroll to the bottom, in case we're in a tall textarea
-    // (Necessary for Firefox and Google Chrome)
-    this.scrollTop = 999999;
-  });
-};
+(function($)
+{
+    jQuery.fn.putCursorAtEnd = function()
+    {
+    return this.each(function()
+    {
+        $(this).focus()
+
+        // If this function exists...
+        if (this.setSelectionRange)
+        {
+        // ... then use it
+        // (Doesn't work in IE)
+
+        // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+        var len = $(this).val().length * 2;
+        this.setSelectionRange(len, len);
+        }
+        else
+        {
+        // ... otherwise replace the contents with itself
+        // (Doesn't work in Google Chrome)
+        $(this).val($(this).val());
+        }
+
+        // Scroll to the bottom, in case we're in a tall textarea
+        // (Necessary for Firefox and Google Chrome)
+        this.scrollTop = 999999;
+    });
+    };
+})(jQuery);
