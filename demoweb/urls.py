09acc52ae2d00c8ea23aaaa23bbc812eff5e77b4
Suraj Pai <surajballambat@gmail.com>
2018-04-25 19:26:00 +0530

    Fix for demo django format

--- demoweb/urls.py
+++ demoweb/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.demo_index, name='demo_home')
+    url(r'^$', views.demo_index, name='demo_home'),
+    url(r'^canny/', views.canny, name='demo_canny')
+
 ]
--- demoweb/views.py
+++ demoweb/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 def demo_index(request):
     return render(request, 'demo/index.html')
+
+
+def canny(request):
+    return render(request, 'demo/video.html')
